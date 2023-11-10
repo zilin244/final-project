5eccc0904c67204af8e914e5d44b11c0fd26838e
zilin244 <148887865+zilin244@users.noreply.github.com>
2023-11-10 15:09:15 +0800

    Update 05Attraction index.py

--- pages/05Attraction index.py
+++ pages/05Attraction index.py
@@ -12,7 +12,7 @@ df_t_num = df[['Attraction Index','Price','Person Capacity','Superhost','Multipl
 #Quality correlation matrix
 k = 11 #number of variables for heatmap
 ## Get the k most correlated in absolute value
-cols = df_t_num.corr().abs().nlargest(k+1, 'Guest Satisfaction')['Guest Satisfaction'].index
+cols = df_t_num.corr().abs().nlargest(k+1, 'Attraction Index')['Attraction Index'].index
 cm = df_t_num[cols].corr()
 
 # create the heatmap
