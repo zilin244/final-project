cdcaf1da46b0372840bc6fcb017bc9fdc43e02ae
zilin244 <148887865+zilin244@users.noreply.github.com>
2023-11-10 14:24:56 +0800

    Update 06City Centre.py

--- pages/06City Centre.py
+++ pages/06City Centre.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-st.title(":map: City Center vs Attraction Index")
+st.title(" :global: City Center vs Attraction Index")
 st.write("👉You can look at the relationship between ") 
 st.write("👉Attraction Index and distance between homestays and city center") 
 
