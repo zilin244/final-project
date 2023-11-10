628383a11ad16c4c8a3b02e83e30ef13ad99fa76
zilin244 <148887865+zilin244@users.noreply.github.com>
2023-11-10 11:34:34 +0800

    Update 04Service.py

--- pages/04Service.py
+++ pages/04Service.py
@@ -17,22 +17,12 @@ st.set_page_config(page_title="Average price per person", page_icon=":bar_chart:
 st.title(":smile: Service")
 st.markdown("### In this page, the level of serive across different city is persented. ")
 
-
-fig, ax = plt.subplots()
-
-df[df['Price']<2000].boxplot(column=['Price'],by = ['City'],ax=ax,meanline=True,showmeans=True);
-
-fig.suptitle("")
-plt.xticks(rotation=60)
-## Now we can actually set the Title
-plt.title("Price by Different City")
-
 df1=df[['Cleanliness Rating','Superhost','Metro Distance (km)','Normalised Restraunt Index']]
 
 l = df1.columns.values ## List of the variables from the columns
 number_of_columns= len(l) ## For the plot
 number_of_rows = 1 ## For the plot
-
+fig, ax = plt.subplots()
 plt.subplots(figsize=(2*number_of_columns,10*number_of_rows))
 for i in range(0, number_of_columns):
     plt.subplot(number_of_rows + 1,number_of_columns,i+1)
